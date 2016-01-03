a5e167093a606922f4c1a7f639869e7420a1936c
Veaceslav Cotruta <Veaceslav.Cotruta@webtrekk.com>
2016-01-03 12:53:24 +0100

    Support dot notation

--- package.js
+++ package.js
@@ -11,10 +11,14 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('jagi:astronomy');
   api.addFiles('lib/modules/events/global.js');
+  api.addFiles('lib/modules/events/before_init.js');
   api.addFiles('lib/modules/events/before_change.js');
   api.addFiles('lib/modules/events/after_set.js');
   api.addFiles('lib/modules/methods/global.js');
   api.addFiles('lib/modules/methods/translate.js');
+  api.addFiles('lib/modules/methods/locales.js');
+  api.addFiles('lib/modules/methods/fields.js');
+  api.addFiles('lib/modules/methods/get.js');
   api.addFiles('lib/modules/fields/types/babelfish.js');
   api.addFiles('lib/modules/behaviors/babelfish.js');
 });
